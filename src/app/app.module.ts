e39ffe9c9d1c411efd1fc3b6edc61091d38543ec
System Administrator <root@Onders-MBP.fritz.box>
2019-11-15 10:45:27 +0100

    working on navigation bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavigationbarComponent } from "./components/navigationbar/navigationbar.component";
+import { HamburgerIconComponent } from "./components/hamburger-icon/hamburger-icon.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NavigationbarComponent, HamburgerIconComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/hamburger-icon/hamburger-icon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hamburger-icon',
+  templateUrl: './hamburger-icon.component.html',
+  styleUrls: ['./hamburger-icon.component.css']
+})
+export class HamburgerIconComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
