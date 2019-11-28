bc45b83c92e4b5092adf1a0fb1ead2a666d07d55
oender kerem <oender.yaz@outlook.com>
2019-11-28 13:23:40 +0100

    imlemented carousel

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,9 +9,14 @@ import { Recommendation } from "../../models/Recommendation";
 })
 export class HomeComponent implements OnInit {
   recommendations: Recommendation[];
+
   firstRecommendation: Recommendation;
   secondRecommendation: Recommendation;
   thirdRecommendation: Recommendation;
+
+  leftRecommendation: Recommendation;
+
+  recommendationIndex = 0;
 
   constructor(private http: HttpClient) {
     this.http
@@ -24,18 +29,89 @@ export class HomeComponent implements OnInit {
   onRecommendationsLoaded = function(data: Object) {
     this.mapRecommendations(data);
     this.initializeRecommendationsToShow();
+    this.startCarousel();
   };
 
   mapRecommendations = function(objects: Object) {
     this.recommendations = objects as Recommendation[];
   };
 
+  switchRecommendation = function(
+    recommendation: Recommendation
+  ): Recommendation {
+    switch (recommendation.className) {
+      case "left":
+        return {
+          ...this.leftRecommendation,
+          className: "right"
+        };
+      case "middle":
+        return {
+          ...recommendation,
+          className: "left"
+        };
+      case "right":
+        return {
+          ...recommendation,
+          className: "middle"
+        };
+      default:
+        return undefined;
+    }
+  };
+
+  startCarousel = function() {
+    setInterval(() => {
+      this.setLeftRecommendation();
+      this.firstRecommendation = this.switchRecommendation(
+        this.firstRecommendation
+      );
+      this.secondRecommendation = this.switchRecommendation(
+        this.secondRecommendation
+      );
+      this.thirdRecommendation = this.switchRecommendation(
+        this.thirdRecommendation
+      );
+      console.log("############");
+      console.log("first", this.firstRecommendation.className);
+      console.log("second", this.secondRecommendation.className);
+      console.log("third", this.thirdRecommendation.className);
+    }, 4000);
+  };
+
+  setLeftRecommendation = function() {
+    let tmpIndex = this.recommendationIndex + 1;
+
+    if (tmpIndex > this.recommendations.length - 1) {
+      this.recommendationIndex = 0;
+    } else {
+      this.recommendationIndex = tmpIndex;
+    }
+    this.leftRecommendation = this.recommendations[this.recommendationIndex];
+    this.leftRecommendation.className = "left";
+  };
+
   initializeRecommendationsToShow = function() {
     if (this.recommendations) {
-      if (this.recommendations.length > 3) {
+      let length = this.recommendations.length;
+
+      if (length >= 3) {
         this.firstRecommendation = this.recommendations[0];
+        this.firstRecommendation.className = "left";
         this.secondRecommendation = this.recommendations[1];
+        this.secondRecommendation.className = "middle";
         this.thirdRecommendation = this.recommendations[2];
+        this.thirdRecommendation.className = "right";
+        this.recommendationIndex = 2;
+      } else if (length > 0) {
+        this.firstRecommendation = this.recommendations[0];
+        if (length >= 1) {
+          this.secondRecommendation = this.recommendations[1];
+        }
+        if (length >= 2) {
+          this.thirdRecommendation = this.recommendations[2];
+        }
+        this.recommendationIndex = length - 1;
       }
     }
   };
