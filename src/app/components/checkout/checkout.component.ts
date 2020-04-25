70c75a4057c5eef436d3f909e20f435bed60d9b8
oender kerem <oender.yaz@outlook.com>
2020-04-25 15:38:39 +0200

    minor changes in checkout -- state of cartitems added

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { State } from "src/app/app.component";
+import { Store } from "@ngrx/store";
+import { ShoppingCartEntry } from "src/app/models/models";
 
 @Component({
-  selector: 'app-checkout',
-  templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.css']
+  selector: "app-checkout",
+  templateUrl: "./checkout.component.html",
+  styleUrls: ["./checkout.component.css"],
 })
 export class CheckoutComponent implements OnInit {
+  constructor(private state: Store<State>) {}
 
-  constructor() { }
+  shoppingCartEntries: ShoppingCartEntry[] = [];
 
   ngOnInit() {
+    this.state
+      .select((store) => store.shoppingCartReducer)
+      .subscribe((data) => this.setCheckoutItems(data.Entries));
   }
 
+  setCheckoutItems(cartEntries: ShoppingCartEntry[]) {
+    if (cartEntries) {
+      this.shoppingCartEntries = cartEntries;
+    }
+  }
 }
